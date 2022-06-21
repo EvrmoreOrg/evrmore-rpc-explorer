98b9ceb4a6bd84a31780babfe53ad30499af91cd
Hans Schmidt <hans_schm1dt@protonmail.com>
2022-06-21 00:35:13 +0000

    Add support for "app/coins/evr.js" file

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,11 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
+var ltc = require("./coins/evr.js");
 
 module.exports = {
+	"EVR": evr,
 	"BTC": btc,
 	"LTC": ltc,
 
-	"coins":["BTC", "LTC"]
+	"coins":['EVR", "BTC", "LTC"]
 };
